3b74f41a9ea5248f909cf26a3d5e52e502f730f1
isabelamurcia <141078742+isabelamurcia@users.noreply.github.com>
2023-08-03 00:01:31 +0000

    ejercicio 2 de agosto

--- main.py
+++ main.py
@@ -1,4 +1,69 @@
 #hola
-#Ejercicio 1:
-print('Hola Mundo')
+#Ejercicio 1
+'''print('hola mundo')'''
 
+#ejercicio 2
+'''palabra=('Hola Mundo')
+print(palabra)
+print('¿cómo te llamas?')'''
+
+'''#ejercicio 3
+nombre=input()
+print('es un placer,'+nombre)'''
+
+#ejercicio 4
+'''print(((3+2)/(2*5))**2)'''
+
+#ejercicio 5 
+'''print('horas trabajadas')
+horas=float(input())
+print('cuanto es el costo por hora')
+costo=float(input())
+print(costo*horas)'''
+
+#ejercicio 6
+'''print('introducir un entero positivo')
+N=int(input())
+print((N*(N+1))/2)'''
+
+#ejercicio 7
+'''print('Cual es su peso(kg)')
+peso=float(input())
+print('cual es su estatura (en metros)')
+estatura=float(input())
+im=((peso/estatura)**2) 
+imc=round(im, 2)
+print('su imc es', imc)'''
+
+#ejercicio 8
+'''print('escriba un numero entero')
+N=int(input())
+print('escriba un numero entero')
+M=int(input())
+C=(N//M)
+R=(N%M)
+print('el cociente es', C)
+print('el residuo es', R)'''
+
+#ejercicio 9
+'''print('escriba una cantidad a invertir')
+c=float(input())
+print('escriba su interes anual')
+i=float(input())
+print('escriba el numero de años')
+años=float(input())
+f=(c*(i/100+1)**años)
+print('inversion')
+print(f)'''
+
+#ejercicio 10
+'''print('numero de payasos')
+p=int(input())
+print('numero de muñecas')
+m=int(input())
+pg=(p*112)
+print('peso total de payasos', pg)
+mg=(m*75)
+print('peso total de muñecas', mg)
+pgmg=(pg+mg)
+print('peso total de payasos y muñecas', pgmg)'''
