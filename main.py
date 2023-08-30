d1984d6e54d69b606c2cdee7a37499b2112b4935
isabelamurcia <141078742+isabelamurcia@users.noreply.github.com>
2023-08-30 22:31:52 +0000

    ver

--- main.py
+++ main.py
@@ -195,38 +195,38 @@ ctaAhorro()'''
 
 #ejercicio 2
 '''def menu():
-  print('''ingrese el codigo de la figura
+ #print(ingrese el codigo de la figura
 circulo 1
 cuadrado 2
 triangulo 3
 pentagono regular 4
-rombo 5''')
-'''def circulo():
+rombo 5)
+#''''''def circulo():
   radio=float(input('igrese el valor del radio '))
   areacirculo=3.1416*radio**2
-  return(areacirculo)
-def cuadrado():
+  return(areacirculo)'''
+'''def cuadrado():
   lado=float(input('ingrese el valor del lado '))
   areacuadrado=lado*lado
-  return(areacuadrado)
-def triangulo():
+  return(areacuadrado)'''
+'''def triangulo():
   base=float(input('ingrese el valor de la base '))
   altura=float(input('ingrese el valor de la altura '))
   areatriangulo=((base*altura)/2)
-  return(areatriangulo)
-def pentagonoregular():
+  return(areatriangulo)'''
+'''def pentagonoregular():
   perimetro=float(input('ingrese el valor del perimetro '))
   apotema=float(input('ingrese el valor del apotema '))
   areapentagonoregular=((perimetro*apotema)/2)
-  return(areapentagonoregular)
-def rombo():
+  return(areapentagonoregular)'''
+'''def rombo():
   diagonalmayor=float(input('ingrese el valor de la diagonal mayor '))
   diagonalmenor=float(input('ingrese el valor de la diagoal menor '))
   if diagonalmenor>diagonalmayor:
     arearombo=print('valores no validos')
   else: arearombo=((diagonalmayor*diagonalmenor)/2)
-  return(arearombo)
-def areafig():
+  return(arearombo)'''
+'''def areafig():
   menu()
   figura=int(input('ingrese el codigo de la figura que desee '))
   if figura==1:
@@ -239,14 +239,37 @@ def areafig():
     print(pentagonoregular())
   elif figura==5:
     print(rombo())
-  else: print('coigo no valido')
-areafig()'''
+  else: print('codigo no valido')'''
+#areafig()
 
 #ejercicio 3
+def menu():
+  print('elija la opcion que se acople a su compra a) 2,000,000 o mas b) 2,000,000 o mas y es marca NOSY c) si solo es marca NOSY')
 
+def a():
+  valorcompra=float(input('ingrese el valor de su compra'))
+    descuentosiniva=valorcompra*0.1
+    iva=descuentosiniva*0.2
+    descuentototal=descuentosiniva+iva
+  return descuentototal
 
+def b():
+  valorcompra=float(input('ingrese el valor de su compra '))
+    descuentosiniva=valorcompra*0,15
+    IVA=descuentosiniva*0.2
+    descuentototal=descuentosiniva+IVA
+  return descuentototal
 
+def c():
+  valorcompra=float(input('ingrese el valor de su compra'))
+    descuentosiniva=valorcompra*0.05
+    IVA=descuentosiniva*0.2
+    descuentototal=descuentosiniva+IVA
+  return descuentototal
 
-
-
-
+def principal():
+  menu()
+  input('ngrese la letra que le corresponde')
+  if letra==a
+    print(a)
+  
